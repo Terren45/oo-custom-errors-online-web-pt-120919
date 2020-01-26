0d02fe122012abf0fa2c3fd9afa28c2fd5c7ca56
Terren Culberson <culbersonterren@yahoo.com>
2020-01-26 04:05:58 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,7 +10,7 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person begin
-      raise PartnerError
+      raise PartnerError 
     else
       person.partner = self
   end
